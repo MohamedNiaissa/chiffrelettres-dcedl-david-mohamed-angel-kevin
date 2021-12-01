5416fe33256547e9e39461c3a129d6295b3cdb1e
Mohamed <mohameddrame907@gmail.com>
2021-12-01 14:37:34 +0100

    Lecture du fichier et assignation des mots dans un tableau

---
+++ src/com/company/RecupMots.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RecupMots {
+
+    public static void extraireMots(){
+
+        ArrayList<String> listeMots = new ArrayList<>();
+
+        try
+        {
+            // Le fichier d'entrée
+            File file = new File("/Users/mohamed/Desktop/ProjetAlgoAvancee/src/com/company/liste_francais.txt");
+            // Créer l'objet File Reader
+            FileReader fr = new FileReader(file);
+            // Créer l'objet BufferedReader
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                // ajoute la ligne au buffer
+                listeMots.add(line);
+            }
+            fr.close();
+
+
+            System.out.println(" Liste mots : " + listeMots);
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+
+
+}
