01a1caac17bc1cee7b2db3e62a8b7d030e7defdd
David <david.roquain@edu.esiee-it.fr>
2021-12-01 14:57:40 +0100

    Vérification plus ou moins de 30 secondes à la saisie

--- src/com/company/NombreJouer.java
+++ src/com/company/NombreJouer.java
@@ -8,7 +8,13 @@ public class NombreJouer {
             try{
                 Thread.sleep(1000);
                 temps--;
-                System.out.print("-");
+                if (i == 0){
+                    System.out.print("+");
+                } else if (i%10 == 9) {
+                    System.out.print("+");
+                } else {
+                    System.out.print("-");
+                }
             }catch(InterruptedException e) {
                 e.printStackTrace();
             }
@@ -32,6 +38,13 @@ public class NombreJouer {
             case "1" -> {
                 System.out.println("Joueur contre machine");
                 chrono(60);
+                long debut = System.currentTimeMillis();
+                String test30 = Main.scan.nextLine();
+                long fin = System.currentTimeMillis();
+                if (fin-debut > 30000){
+                    System.out.println("Vous avez été trop long");
+                }
+                System.out.println("Résultat enregistré");
             }
             case "2" -> System.out.println("Joueur 1 contre jouer 2");
             case "3" -> {
