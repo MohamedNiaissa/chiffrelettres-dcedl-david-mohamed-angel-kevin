454624ae11640ee7263a9496601571832caa03c2
Angel Gonzalez <generalpompeyo@hotmail.com>
2021-12-02 21:39:55 +0100

    Optimisation: - Nombre de lignes par classe reduit considerablement - Melieur lisibilité du code - Menus plus clairs - Print correct de manche pour le deux modes - Changement de message lors de l'impresion du mot saisie - Autres ameliorations mineurs

--- src/com/company/LancerJeu.java
+++ src/com/company/LancerJeu.java
@@ -1,45 +1,48 @@
 package com.company;
-
 import java.util.ArrayList;
 
 public class LancerJeu {
     public static void menuSelectionMode() {
         System.out.println("""
-                ****************************
-                Menu selection de mode
-                ****************************
-                1) Joueur contre machine
-                2) Joueur 1 contre Joueur 2
-                ****************************
-                3) Revenir au menu principal
+                ***********************************************************
+                ****************** Menu selection de mode *****************
+                ***********************************************************
+                Pour jouer contre la machine tapez -> 1
+                Pour jouer contre un autre jouer tapez -> 2
+                ***********************************************************
+                Pour revenir au menu principal tapez -> 3
+                ***********************************************************
                 """);
 
         String choixJouer = "";
         choixJouer = Main.scan.nextLine();
+
         switch (choixJouer) {
             case "1" -> {
-                System.out.println("Joueur contre machine");
-                ArrayList<String> tablettre = Lettre.partieLettreUnJoueur();
-                Chrono.chrono(5);
-                String reponseUtilisateur = tempsJoueur.tempsJoueurReponseLettre();
-                ArrayList<String> listMots = RecupMots.extraireMots();
-                Verification.verificateurReponseMot(listMots,tablettre,reponseUtilisateur);
-                String reponseMachine = RechercheMotLong.motLong(listMots,tablettre);
-
-               /* long debut = System.currentTimeMillis();
-                long fin = System.currentTimeMillis();
-                if (fin-debut > 30000){
-                    System.out.println("Vous avez été trop long");
+                int manche = 1;
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("\nManche : " + manche);
+                    // Lettres
+                    ArrayList<String> tablettre = Lettre.partieLettreUnJoueur();
+                    Chrono.chrono(2);
+                    String reponseUtilisateur = tempsJoueur.tempsJoueurReponseLettre();
+                    manche +=1;
+                    ArrayList<String> listMots = RecupMots.extraireMots();
+                    Verification.verificateurReponseMot(listMots,tablettre,reponseUtilisateur);
+                    String reponseMachine = RechercheMotLong.motLong(listMots,tablettre);
                 }
-                System.out.println("Résultat enregistré"); */
             }
             case "2" -> {
-                System.out.println("Joueur 1 contre jouer 2");
-                ArrayList<String> tablettre = Lettre.partieDeuxJoueurs();
-
-
+                int manche = 1;
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("\nManche : " + manche);
+                    // Lettres
+                    ArrayList<String> tablettre = Lettre.partieDeuxJoueurs();
+                    Chrono.chrono(10);
+                    String reponseUtilisateur = tempsJoueur.tempsJoueurReponseLettre();
+                    manche +=1;
+                }
             }
-
             case "3" -> {
             }
             default -> {
