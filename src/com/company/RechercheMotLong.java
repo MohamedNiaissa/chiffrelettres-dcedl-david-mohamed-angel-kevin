83f7bc146fb8f5598c20fd9b023a8390681c68c5
Angel Gonzalez <generalpompeyo@hotmail.com>
2021-12-02 21:53:26 +0100

    Optimisation: - Nombre de lignes pour la classe RechercheMotLong

--- src/com/company/RechercheMotLong.java
+++ src/com/company/RechercheMotLong.java
@@ -1,92 +1,68 @@
 package com.company;
-
 import java.util.ArrayList;
 
 public class RechercheMotLong {
-
-    public static String motLong(ArrayList<String> tabMots,ArrayList<String> tabLettres){
-
+    public static String motLong(ArrayList<String> tabMots,ArrayList<String> tabLettres) {
         int compteur = 0;
-
         ArrayList<String> motsValide = new ArrayList<>();
         ArrayList<Character> decoupeMot = new ArrayList<>();
         ArrayList<String> decoupeMotString = new ArrayList<>();
 
-
         for (int i = 0; i <tabMots.size() ; i++) {
             ArrayList<String> tabLettresclone = new ArrayList<>();
-
-
             for (int k = 0; k < tabMots.get(i).length(); k++) {
-
                 decoupeMot.add(tabMots.get(i).toLowerCase().charAt(k));
-
             }
-
 
             for (int h = 0; h < decoupeMot.size(); h++) {
                 String str = Character.toString(decoupeMot.get(h));
                 decoupeMotString.add(str);
             }
 
-
             for (int h = 0; h < tabLettres.size(); h++) {
                 tabLettresclone.add(tabLettres.get(h));
 
             }
+
             for (int j = 0; j < decoupeMotString.size(); j++) {
-
                 for (int l = 0; l < tabLettresclone.size() ; l++) {
-
                     if (decoupeMotString.get(j).equals(tabLettresclone.get(l))){
-
                         compteur++;
                         tabLettresclone.remove(l);
                         break;
-
                     }
-
                 }
-
             }
-            if (compteur == decoupeMotString.size()){
+
+            if (compteur == decoupeMotString.size()) {
                 motsValide.add(tabMots.get(i));
             }
+
             decoupeMot = new ArrayList<>();
             decoupeMotString = new ArrayList<>();
-
             compteur = 0;
-
         }
 
-
         /* Trouver le mot le plus long*/
-
         String motLePlusLongprovi = "";
         String motLePlusLong = "";
 
         for (int u = 0; u < motsValide.size(); u++) {
-
             for (int i = 0; i < motsValide.size()-1; i++) {
                 if (motsValide.get(u).length() >= motsValide.get(i).length()){
                     motLePlusLongprovi = motsValide.get(u);
-                }else {
+                } else {
                     motLePlusLongprovi = motsValide.get(i);
                 }
-
-            }
+             }
 
             if ( motLePlusLong.length() >= motLePlusLongprovi.length()){
                 motLePlusLong = motLePlusLong;
-            }else {
+            } else {
                 motLePlusLong = motLePlusLongprovi;
             }
+         }
 
-        }
-
-
-        return motLePlusLong;
-
-    }
-
-}
+         return motLePlusLong;
+     }
+ }
